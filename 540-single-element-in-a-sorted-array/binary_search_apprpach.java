b8e5a56f59f97cbef3381de9edb5f4a235fa5323
CheneyDev <cheneydev@gmail.com>
2023-02-21 22:34:14 +0800

    #540

---
+++ 540-single-element-in-a-sorted-array/binary_search_apprpach.java
@@ -0,0 +1,25 @@
+
+public class binary_search_apprpach {
+    public int singleNonDuplicate(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+        if(nums.length==1) return nums[0];
+        if(nums[low]!=nums[low+1]) return nums[low];
+        if(nums[high]!=nums[high-1]) return nums[high];
+
+        while(low<high){
+
+            int mid=(high+low)/2;
+            
+            if(mid%2==1){
+                mid--;
+            }
+            if(nums[mid]!=nums[mid+1]){
+                high=mid;
+            }else{
+                low=mid+2;
+            }
+        }
+        return nums[low];
+    }
+}
