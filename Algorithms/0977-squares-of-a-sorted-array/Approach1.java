92bdb098b5a8f4a5646f96e475723be23619ece3
CheneyDev <cheneydev@gmail.com>
2023-03-16 23:05:22 +0800

    #0977

---
+++ Algorithms/0977-squares-of-a-sorted-array/Approach1.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/squares-of-a-sorted-array/solutions/221922/java-two-pointers-o-n/?envType=study-plan&id=algorithm-i&orderBy=hot
+//Java two pointers
+
+class Approach1 {
+    public int[] sortedSquares(int[] nums) {
+        int numsLength = nums.length;
+        int[] newNums = new int[numsLength];
+        int p1 = 0;
+        int p2 = numsLength - 1;
+        for (int i = numsLength-1; i >= 0; i--) {
+            if (Math.abs(nums[p1]) > Math.abs(nums[p2])) {
+                newNums[i] = nums[p1] * nums[p1];
+                p1++;
+            } else {
+                newNums[i] = nums[p2] * nums[p2];
+                p2--;
+            }
+        }
+        return newNums;
+    }
+}
