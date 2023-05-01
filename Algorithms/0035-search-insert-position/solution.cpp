0faa3008fc027a329bf3eb342b883eefe5bbb40f
CheneyDev <cheneydev@gmail.com>
2023-05-01 19:19:30 +0800

    some changes

--- Algorithms/0035-search-insert-position/solution.cpp
+++ Algorithms/0035-search-insert-position/solution.cpp
@@ -3,13 +3,13 @@ class Solution
 public:
     int searchInsert(vector<int> &nums, int target)
     {
-        int start=0;
-        int end=nums.size()-1;
+        int start = 0;
+        int end = nums.size() - 1;
         int mid;
 
         while (start <= end)
         {
-            mid=(start+end)/2;
+            mid = (start + end) / 2;
             if (nums[mid] == target)
             {
                 return mid;
