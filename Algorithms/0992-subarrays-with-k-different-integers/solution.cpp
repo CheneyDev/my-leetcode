59a737fe35c4d117d9c2c2cd0cfc794d32a5cdad
CheneyDev <cheneydev@gmail.com>
2023-08-04 18:50:32 +0800

    992:sliding window and hashmap

--- Algorithms/0992-subarrays-with-k-different-integers/solution.cpp
+++ Algorithms/0992-subarrays-with-k-different-integers/solution.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int left=0;
-        unordered_map<int,int> count;
-        int distinct_count =0;
-        int result=0;
+        return helper(nums,k)-helper(nums,k-1);
+    }
 
-        for(int right=0;right<nums.size();right++){
+    int helper(vector<int>& nums,int k){
 
-            count[nums[right]]++;
-
-            if(count[nums[right]]==1){
-                distinct_count++;
-            }
-
-            if(distinct_count>k){
-                count[nums[left]]--;
-                if(count[nums[left]]==0){
-                    distinct_count--;
+        int n=nums.size();
+        unordered_map<int,int> map;
+        int start=0,end=0,count=0;
+        while(end<n){
+            map[nums[end]]++;
+            while(map.size()>k){
+                map[nums[start]]--;
+                if(map[nums[start]]==0){
+                    map.erase(nums[start]);
                 }
-                left++;
+                start++;
             }
-
-            if(distinct_count==k){
-                result+=right-left+1;
-            }
-
+            count+=(end-start-1);
+            end++;
         }
-
-        return result;
+        return count;
     }
 };
