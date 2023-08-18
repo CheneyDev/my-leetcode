2498603b43f94591b4f2014940b310d3ef16fd03
CheneyDev <cheneydev@gmail.com>
2023-08-18 22:25:45 +0800

    some changes

--- Algorithms/list/0002-add-two-numbers/solution.cpp
+++ Algorithms/list/0002-add-two-numbers/solution.cpp
@@ -8,36 +8,29 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
+class Solution {
 public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
-    {
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         // 创建结果连表的虚拟头节点
-        ListNode *dummy = new ListNode(0);
+        ListNode* dummy=new ListNode(0);
         // 创建当前节点
-        ListNode *current = dummy;
+        ListNode* current=dummy;
 
-        int x, y, sum;
-        int carry = 0;
-        // 遍历两个链表
-        while (l1 || l2)
-        {
-            x = (l1) ? l1->val : 0;
-            y = (l2) ? l2->val : 0;
-            sum = x + y + carry;
-            carry = sum / 10;
-            current->next = new ListNode(sum % 10);
-            current = current->next;
-            if (l1)
-                l1 = l1->next;
-            if (l2)
-                l2 = l2->next;
+        int x,y,sum;
+        int carry=0;
+        while(l1||l2){
+            x=(l1)?l1->val:0;
+            y=(l2)?l2->val:0;
+            sum=x+y+carry;
+            carry=sum/10;
+            current->next=new ListNode(sum%10);
+            current=current->next;
+            if(l1) l1=l1->next;
+            if(l2) l2=l2->next;
         }
 
-        if (carry != 0)
-        {
-            current->next = new ListNode(carry);
+        if(carry!=0){
+            current->next=new ListNode(carry);
         }
 
         return dummy->next;
