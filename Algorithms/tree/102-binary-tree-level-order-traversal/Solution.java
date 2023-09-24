a8ba9154096b929224760382b448737dac041f85
CheneyDev <cheneydev@gmail.com>
2023-09-24 22:28:34 +0800

    102

--- Algorithms/tree/102-binary-tree-level-order-traversal/Solution.java
+++ Algorithms/tree/102-binary-tree-level-order-traversal/Solution.java
@@ -9,5 +9,21 @@ class Solution{
         Queue<TreeNode> queue=new LinkedList<>();
         queue.offer(root);
 
+        while(!queue.isEmpty()){
+            int levelSize=queue.size();
+            List<Integer> levelResult=new ArrayList<>();
+            for(int i=0;i<levelSize;i++){
+                TreeNode node=queue.poll();
+                levelResult.add(node.val);
+                if(node.left!=null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(levelResult);
+        }
+return result;
     }
 }
