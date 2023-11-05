48b8b7ee18ecc8a4d78ef103d8eaacfef84355d2
CheneyDev <cheneydev@gmail.com>
2023-11-06 00:01:33 +0800

    101 symmetric tree

---
+++ tree/0101-symmetric-tree/solution.go
@@ -0,0 +1,26 @@
+package _101_symmetric_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	return isMirror(root.Left, root.Right)
+}
+
+func isMirror(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+
+	return left.Val == right.Val && isMirror(left.Left, right.Right) && isMirror(left.Right, right.Left)
+
+}
