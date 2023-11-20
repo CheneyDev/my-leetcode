aec1912e1ee8a66141b67b0d2923240b89edf077
CheneyDev <cheneydev@gmail.com>
2023-11-20 22:13:12 +0800

    235 lowest common ancestor of a binary searrch tree

---
+++ tree/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.go
@@ -0,0 +1,23 @@
+package _235_lowest_common_ancestor_of_a_binary_search_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	for root != nil {
+		if p.Val > root.Val && q.Val > root.Val {
+			root = root.Right
+		} else if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else {
+			return root
+		}
+	}
+	return nil
+}
